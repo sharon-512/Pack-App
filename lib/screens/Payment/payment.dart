8e4e0e10605c391b688d33193e1cf92eba27e491
dathatreyanP <pld.dathan007@gmail.com>
2024-07-31 11:31:53 +0530

    payment back button disabled

--- lib/screens/Payment/payment.dart
+++ lib/screens/Payment/payment.dart
@@ -43,218 +43,230 @@ class _PaymentScreenState extends State<PaymentScreen> {
     final String formattedDate = DateFormat('d MMMM yyyy . hh:mm a').format(DateTime.now());
     final int generatedId = Random().nextInt(10000); // Generates a random ID between 0 and 9999
 
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/payment_bg.png'),
-            fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BottomNavbar(selectedIndex: 0),
           ),
-          gradient: RadialGradient(
-            center: Alignment(0.2698, -0.3198),
-            radius: 2,
-            colors: [
-              Color(0xFF002216), // Dark color
-              Color(0xFF124734), // Light color
-            ],
-            stops: [0.0, 1.0],
+              (route) => false,
+        );
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/payment_bg.png'),
+              fit: BoxFit.cover,
+            ),
+            gradient: RadialGradient(
+              center: Alignment(0.2698, -0.3198),
+              radius: 2,
+              colors: [
+                Color(0xFF002216), // Dark color
+                Color(0xFF124734), // Light color
+              ],
+              stops: [0.0, 1.0],
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Image.asset('assets/images/tick3.png'),
-            const Text(
-              'Order Successful',
-              style: TextStyle(
-                  fontFamily: 'Aeonik',
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                  letterSpacing: -0.41),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 360,
-              width: 310,
-              child: Stack(
-                children: [
-                  Container(
-                    height: 360,
-                    width: 310,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/bill_bg.png'),
-                        fit: BoxFit.fill,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('assets/images/tick3.png'),
+              const Text(
+                'Order Successful',
+                style: TextStyle(
+                    fontFamily: 'Aeonik',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    letterSpacing: -0.41),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 360,
+                width: 310,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 360,
+                      width: 310,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/bill_bg.png'),
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 25, vertical: 25),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              '${widget.subTotal} QR',
-                              style: const TextStyle(
-                                fontFamily: 'Aeonik',
-                                fontWeight: FontWeight.w700,
-                                fontSize: 34,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 25),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                '${widget.subTotal} QR',
+                                style: const TextStyle(
+                                  fontFamily: 'Aeonik',
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 34,
+                                ),
                               ),
-                            ),
-                            Text(
-                              formattedDate,
-                              style: const TextStyle(
-                                color: Color(0xffB6B6B6),
-                                fontFamily: 'Aeonik',
-                                fontWeight: FontWeight.w500,
-                                fontSize: 10,
+                              Text(
+                                formattedDate,
+                                style: const TextStyle(
+                                  color: Color(0xffB6B6B6),
+                                  fontFamily: 'Aeonik',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 10,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Item name',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      'Order NO',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      'Customer Name',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      'Start Date',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      'End Date',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      widget.planName,
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      generatedId.toString(),
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    Text(
+                                      user!.firstname ?? 'name',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14, letterSpacing: -0.14),
+                                    ),
+                                    _isLoading
+                                        ? const CircularProgressIndicator()
+                                        : Text(
+                                      startDate != null
+                                          ? DateFormat('yyyy-MM-dd')
+                                          .format(startDate!)
+                                          : '------',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14,
+                                          letterSpacing: -0.14),
+                                    ),
+                                    _isLoading
+                                        ? const CircularProgressIndicator()
+                                        : Text(
+                                      endDate != null
+                                          ? DateFormat('yyyy-MM-dd')
+                                          .format(endDate!)
+                                          : '------',
+                                      style: CustomTextStyles.labelTextStyle
+                                          .copyWith(
+                                          fontSize: 14,
+                                          letterSpacing: -0.14),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: _shareReceipt,
+                            child: Container(
+                              width: 260,
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xffFBC56D),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
+                                  SvgPicture.asset('assets/images/share.svg'),
                                   Text(
-                                    'Item name',
+                                    '  Share Receipt',
                                     style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    'Order NO',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    'Customer Name',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    'Start Date',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    'End Date',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
+                                        .copyWith(color: Colors.white),
+                                  )
                                 ],
                               ),
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    widget.planName,
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    generatedId.toString(),
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  Text(
-                                    user!.firstname ?? 'name',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14, letterSpacing: -0.14),
-                                  ),
-                                  _isLoading
-                                      ? const CircularProgressIndicator()
-                                      : Text(
-                                    startDate != null
-                                        ? DateFormat('yyyy-MM-dd')
-                                        .format(startDate!)
-                                        : '------',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14,
-                                        letterSpacing: -0.14),
-                                  ),
-                                  _isLoading
-                                      ? const CircularProgressIndicator()
-                                      : Text(
-                                    endDate != null
-                                        ? DateFormat('yyyy-MM-dd')
-                                        .format(endDate!)
-                                        : '------',
-                                    style: CustomTextStyles.labelTextStyle
-                                        .copyWith(
-                                        fontSize: 14,
-                                        letterSpacing: -0.14),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        GestureDetector(
-                          onTap: _shareReceipt,
-                          child: Container(
-                            width: 260,
-                            height: 45,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: const Color(0xffFBC56D),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SvgPicture.asset('assets/images/share.svg'),
-                                Text(
-                                  '  Share Receipt',
-                                  style: CustomTextStyles.labelTextStyle
-                                      .copyWith(color: Colors.white),
-                                )
-                              ],
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const BottomNavbar(selectedIndex: 0),
-                      ));
-                },
-                child: SvgPicture.asset('assets/images/exit.svg')),
-          ],
+              const SizedBox(
+                height: 60,
+              ),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const BottomNavbar(selectedIndex: 0),
+                        ));
+                  },
+                  child: SvgPicture.asset('assets/images/exit.svg')),
+            ],
+          ),
         ),
       ),
     );

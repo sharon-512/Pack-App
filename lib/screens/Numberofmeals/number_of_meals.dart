8479f51591bcf19e64c536787a2ea88d8add250e
dathatreyanP <pld.dathan007@gmail.com>
2024-08-03 14:37:51 +0530

    no data

--- lib/screens/Numberofmeals/number_of_meals.dart
+++ lib/screens/Numberofmeals/number_of_meals.dart
@@ -259,7 +259,7 @@ class _NumberOfMealsState extends State<NumberOfMeals> {
                             builder: (context) => DailyNutrition(
                                 subplanId: widget.subplanId,
                                 mealtypeId: selectedMealType,
-                                numberofMeals: 3),
+                                numberofMeals: totalMeals),
                           ),
                         );
                       } else {
